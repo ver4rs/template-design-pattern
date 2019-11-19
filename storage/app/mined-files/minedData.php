b5563e6bf5dbc9c0045cb0c8d2b44034fa6b794f
Martin Sekerák <martin.sekerak@eset.sk>
2019-11-19 15:51:47 +0100

    s

---
+++ storage/app/mined-files/minedData.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    [
+        "name" => "Abecesa",
+        "description" => "Dfef ewfg ew gw",
+        "price" => 16.25,
+        "quantity" => 22,
+    ],
+    [
+        "name" => "Abecesa 1",
+        "description" => "Dfef ewfg ew gw 1",
+        "price" => 15,
+        "quantity" => 6,
+    ],
+    [
+        "name" => "Abecesa 2",
+        "description" => "Dfef ewfg ew gw 2",
+        "price" => 15,
+        "quantity" => 6,
+    ],
+    [
+        "name" => "Abecesa 3",
+        "description" => "Dfef ewfg ew gw 3",
+        "price" => 15,
+        "quantity" => 6,
+    ],
+    [
+        "name" => "Abecesa 4",
+        "description" => "Dfef ewfg ew gw 4",
+        "price" => 15,
+        "quantity" => 6,
+    ],
+    [
+        "name" => "Abecesa 5",
+        "description" => "Dfef ewfg ew gw 5",
+        "price" => 15,
+        "quantity" => 6,
+    ],
+    [
+        "name" => "Abecesa 6",
+        "description" => "Dfef ewfg ew gw 6",
+        "price" => 15,
+        "quantity" => 6,
+    ],
+    [
+        "name" => "Abecesa 7",
+        "description" => "Dfef ewfg ew gw 7",
+        "price" => 15,
+        "quantity" => 6,
+    ],
+];
